0f9e0055a2736c633d5604b8efefabbf5797b3c4
abendafi <bendafischulmann@gmail.com>
2025-06-03 15:12:17 +0200

    fix

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 export const CHAINS = {
-    HARDHAT : { id: 31337,    name: "Hardhat"  },
-    SEPOLIA : { id: 11155111, name: "Sepolia"  },
-} as const;
+    HARDHAT : { id: 31337,   name: "Hardhat" },
+    SEPOLIA : { id: 11155111, name: "Sepolia" }
+};
